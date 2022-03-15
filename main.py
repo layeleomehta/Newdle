bea02e68aef3189a323355c61ac7d42eed7b94b1
Laye 'Leo' Mehta <layeamehta@gmail.com>
2022-03-15 12:51:19 -0400

    added generate word function, and finished main function

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-""" Python script to test functionality """
-
 import itertools
 from nltk.corpus import wordnet 
 from nltk.corpus import words
+from random_word import RandomWords
+
+# returns a randomly generated 5 letter English word
+def generate_word():
+    r = RandomWords()
+    new_word = r.get_random_word(minLength=5, maxLength=5)
+    new_word = new_word.lower()
+
+    return new_word
 
 
-# returns list of all permutations of input word; starting from length 3 to length of word. 
-def make_word_permutations(word):
+# return True if input word is a valid English word, False otherwise.  
+def validate_word(word):
+    if(word in wordnet.words() and words.words().__contains__(word)):
+        return True
+    else:
+        return False
+
+# returns list of all valid English permutations of input word; starting from length 3 to length of word. 
+def make_valid_permutations(word):
     if(len(word) <= 2):
         print("Please input a word with length greater than 2 letters")
         return
@@ -23,19 +37,20 @@ def make_word_permutations(word):
 
     return list(combos)
 
-# return True if input word is a valid English word, False otherwise.  
-def validate_word(word):
-    if(word in wordnet.words() and words.words().__contains__(word)):
-        return True
-    else:
-        return False
-
 def main():
-    word = "cabin"
-    print(make_word_permutations(word))
+    output = []
+    word = ""
+    num_valid_words = 0
 
+    while(num_valid_words < 3 or num_valid_words > 7):
+        word = generate_word()
+        output = make_valid_permutations(word)
+        num_valid_words = len(output)
     
-
+    print("Word:", word)
+    print("All combinations:", output)
+    print("Number of combinations:", num_valid_words)
+    return (word, output, num_valid_words)
 
 if __name__ == "__main__":
     main()
