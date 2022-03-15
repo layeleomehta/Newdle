a3ffcaa394b168441f77fe2d2b94e94b6c11c61b
Laye 'Leo' Mehta <layeamehta@gmail.com>
2022-03-15 12:27:26 -0400

    added functionality to validate English word

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 """ Python script to test functionality """
 
 import itertools
+from nltk.corpus import wordnet 
+from nltk.corpus import words
 
-# generate all permutations of input word; starting from length 3 to length of word. 
+
+# returns list of all permutations of input word; starting from length 3 to length of word. 
 def make_word_permutations(word):
     if(len(word) <= 2):
         print("Please input a word with length greater than 2 letters")
         return
-
+    
+    word = word.lower()
     wordList = list(word)
     combos = set()
 
-    for i in range(0, len(wordList)+1):
+    for i in range(3, len(wordList)+1):
         for subset in itertools.permutations(wordList, i):
             str_subset = "".join(subset)
-            if(len(str_subset) > 2 and str_subset != word):
+            if(validate_word(str_subset) and str_subset != word):
                 combos.add(str_subset)
+
     return list(combos)
 
+# return True if input word is a valid English word, False otherwise.  
+def validate_word(word):
+    if(word in wordnet.words() and words.words().__contains__(word)):
+        return True
+    else:
+        return False
+
 def main():
-    word = "fancy"
+    word = "cabin"
     print(make_word_permutations(word))
 
     
