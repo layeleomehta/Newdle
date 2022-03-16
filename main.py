4b82e77783ae899f8d5003e6570dda03268b9a25
Laye 'Leo' Mehta <layeamehta@gmail.com>
2022-03-15 22:07:25 -0400

    created boilerplate for UI and added definitions functionality to python script

--- main.py
+++ main.py
@@ -1,30 +1,43 @@
 import itertools
-from nltk.corpus import wordnet 
+from hypothesis import example
+from nltk.corpus import wordnet
 from nltk.corpus import words
 from random_word import RandomWords
+import json
+import time
 
 # returns a randomly generated 5 letter English word
 def generate_word():
     r = RandomWords()
     new_word = r.get_random_word(minLength=5, maxLength=5)
-    new_word = new_word.lower()
+    if(new_word is not None):
+        new_word = new_word.lower()
+        return new_word
+    else:
+        return ""
 
-    return new_word
 
-
-# return True if input word is a valid English word, False otherwise.  
+# return True if input word is a valid English word, False otherwise.
 def validate_word(word):
     if(word in wordnet.words() and words.words().__contains__(word)):
         return True
     else:
         return False
 
-# returns list of all valid English permutations of input word; starting from length 3 to length of word. 
+# return the definition of passed in word
+def get_definition(word):
+    if(wordnet.synsets(word)):
+        syns = wordnet.synsets(word)
+        return syns[0].definition()
+    else:
+        return ""
+
+# returns list of all valid English permutations of input word; starting from length 3 to length of word.
 def make_valid_permutations(word):
     if(len(word) <= 2):
         print("Please input a word with length greater than 2 letters")
         return
-    
+
     word = word.lower()
     wordList = list(word)
     combos = set()
@@ -38,21 +51,39 @@ def make_valid_permutations(word):
     return list(combos)
 
 # generates random 5 letter word, finds all valid English permutations
-# returns a tuple of the random word, a list of valid combinations and number of combinations. 
+# returns a tuple.
 def main():
     output = []
     word = ""
+    definitions = []
     num_valid_words = 0
 
-    while(num_valid_words < 3 or num_valid_words > 7):
+    while(num_valid_words < 3 or num_valid_words > 6):
         word = generate_word()
         output = make_valid_permutations(word)
         num_valid_words = len(output)
     
-    return (word, output, num_valid_words)
+    for i in range(0, len(output)):
+        definitions.append(get_definition(output[i]))
+
+    return (word, output, definitions, num_valid_words)
+
 
 if __name__ == "__main__":
-    word, output, num_valid_words = main()
-    print("Word:", word)
-    print("All combinations:", output)
-    print("Number of combinations:", num_valid_words)
+    exampleList = []
+    
+    for i in range(0, 5):
+        print("Start iteration number:", i)
+        word, output, definitions, num_valid_words = main()
+        data = {}
+        data['word'] = word
+        data['combinations'] = output
+        data['definitions'] = definitions
+        data['numCombinations'] = num_valid_words
+        jsonObj = json.dumps(data)
+        exampleList.append(jsonObj)
+        print("Done iteration number:", i)
+        time.sleep(3)
+
+
+    print(exampleList)
