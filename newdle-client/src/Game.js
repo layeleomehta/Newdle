2e3c63158a2fea8c59d0045c19c1d829b29177ae
Laye 'Leo' Mehta <layeamehta@gmail.com>
2022-03-25 15:34:05 -0400

    added data in constants folder

--- newdle-client/src/Game.js
+++ newdle-client/src/Game.js
@@ -1,23 +1,42 @@
 import React, { useState } from 'react';
-
-function getNum(num){
-    return num+1; 
-}
-
-let myObj = getNum(0)
-
+import RICIBs from 'react-individual-character-input-boxes';
+import { wordList } from './constants/words';
 
 function Game(props) { 
-    const [myNum, setmyNum] = useState(myObj);
+    const [outString, setOutString] = useState("");
+
+    // generates random word from the wordList
+    const getRandomWord =()=>{
+        var alphabetIndex = Math.floor(Math.random() * 26);
+        var wordIndex = Math.floor(Math.random() * wordList[String.fromCharCode(97 + alphabetIndex)].length);
+        var randomWord = wordList[String.fromCharCode(97 + alphabetIndex)][wordIndex];
+        return randomWord; 
+      }
+
+    const handleRICIBsOutput = (string) => {
+        console.log(string); 
+    }
+    // Renders 'num' number of squares for text input
+    const renderSquares = (num) => {
+        var words = []
+        for(let i=0; i<num; i++){
+            words.push(<span key={i}>        
+                <RICIBs
+                amount={5}
+                handleOutputString={handleRICIBsOutput}
+                inputProps={[]}
+                inputRegExp={/^[a-z]$/}
+              /></span>); 
+        }
+        return words; 
+    }
 
     return ( 
-        <div>
-            <h1>This is the Game component!</h1>
-            <h1>{myNum}</h1>
-            <button onClick={() => {setmyNum(myNum+1)}}>Hello</button>
-            <h1>myProp is: {props.myProp}</h1>
-
-        </div>
+    <div>
+        <div>{renderSquares(5)}</div>
+        <button className="reset-board">Click to Reset Word</button>
+        <h2>{outString}</h2>
+    </div>
      );
 }
 
